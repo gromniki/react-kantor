a5ee2fb6c4c642e079c2e34a570baec6df84b9aa
RomanG <gromniki@gmail.com>
2026-07-03 18:00:53 +0300

    refactor: extract Menu and Reviews; fix eslint config for react

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -9,8 +9,8 @@ export const Restaurant = ({ name, menu, reviews }) => {
   return (
     <li>
       <h2>{name}</h2>
-      {menu.length ? <Menu title="Меню" menu={menu} /> : null}
-      {reviews.length ? <Reviews title="Отзывы" reviews={reviews} /> : null}
+      {menu?.length ? <Menu title="Меню" menu={menu} /> : null}
+      {reviews?.length ? <Reviews title="Отзывы" reviews={reviews} /> : null}
     </li>
   );
 };
